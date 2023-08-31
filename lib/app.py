a245d4c433484d4ad222876e7e39d979c1b977a8
SL33PZ <93669183+SL33PZ@users.noreply.github.com>
2023-08-31 18:18:00 +0200

    Add files via upload

---
+++ lib/app.py
@@ -0,0 +1,22 @@
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+import sys
+
+from windowMain import Ui_MainWindow
+
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+        
+        
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec())
